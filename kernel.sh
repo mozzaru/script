e3d72bb7e7e1cb0e159dd949d85dab9bb937e854
mozzaru <rianyanuar52@gmail.com>
2023-12-23 16:09:12 +0700

    script: new script

--- kernel.sh
+++ kernel.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
+*#!/bin/bash
 #
-# Script For Building Android arm64 Kernel
-#
-# Copyright (C) 2021-2023 itsshashanksp <9945shashank@gmail.com>
+# Copyright (C) 2020 Fox kernel project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -30,7 +28,16 @@ rm -rf out
 rm -rf zip
 rm -rf error.log
 
+echo -e "$green << setup dirs >> \n $white"
+
+# With that setup , the script will set dirs and few important thinks
+
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
 # Now u can chose which things need to be modified
+# CHATID = chatid of a telegram group/channel
+# API_BOT = api bot of a telegram bot
 #
 # DEVICE = your device codename
 # KERNEL_NAME = the name of ur kranul
@@ -43,16 +50,17 @@ rm -rf error.log
 # HOSST = build host
 # USEER = build user
 #
+# TOOLCHAIN = the toolchain u want to use "gcc/clang"
 
-# Set main directory of kernel source.
-MAIN_DIR="$(pwd)"
+# CHATID="5662490629"
+# API_BOT="5862606583:AAGDNDgElCTi2i74XNayUsAOKjt3Bl7njGc"
 
 # Devices
 if [ "$DEVICE_TYPE" == markw  ];
 then
-DEVICE="REDMI 4 PRIME"
-KERNEL_NAME="Prototype-v2"
+DEVICE="Redmi 4 Prime"
 CODENAME="markw"
+KERNEL_NAME="Prototype-v2"
 
 DEFCONFIG="markw_defconfig"
 
@@ -60,11 +68,13 @@ AnyKernel="https://github.com/mozzaru/anykernel.git"
 AnyKernelbranch="master"
 fi
 
+HOSST="Show Buildbot"
+USEER="mozzaru"
+
+TOOLCHAIN="clang"
+
 # Kernel build release tag
 KRNL_REL_TAG="$KERNEL_TAG"
-
-HOSST="show-bag"
-USEER="mozzaru"
 
 # setup telegram env
 export BOT_MSG_URL="https://api.telegram.org/bot$API_BOT/sendMessage"
@@ -96,45 +106,76 @@ tg_error() {
         -F caption="$3Failed to build , check <code>error.log</code>"
 }
 
-# clang stuff
-		echo -e "$green << cloning clang >> \n $white"
-		git clone --depth=1 https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r498229b.git  "$HOME"/clang
+# Now let's clone gcc/clang on HOME dir
+# And after that , the script start the compilation of the kernel it self
+# For regen the defconfig . use the regen.sh script
 
-	export PATH="$HOME/clang/bin:$PATH"
-	export KBUILD_COMPILER_STRING=$("$HOME"/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+if [ "$TOOLCHAIN" == gcc ]; then
+	if [ ! -d "$HOME/gcc64" ] && [ ! -d "$HOME/gcc32" ]
+	then
+		echo -e "$green << cloning gcc from arter >> \n $white"
+		git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 "$HOME"/gcc64
+		git clone --depth=1 https://github.com/mvaisakh/gcc-arm "$HOME"/gcc32
+	fi
+	export PATH="$HOME/gcc64/bin:$HOME/gcc32/bin:$PATH"
+	export STRIP="$HOME/gcc64/aarch64-elf/bin/strip"
+	export KBUILD_COMPILER_STRING=$("$HOME"/gcc64/bin/aarch64-elf-gcc --version | head -n 1)
+elif [ "$TOOLCHAIN" == clang ]; then
+	if [ ! -d "$HOME/weebx_clang" ]
+	then
+		echo -e "$green << cloning weebx clang 17 >> \n $white"
+		wget "$(curl -s https://raw.githubusercontent.com/XSans0/WeebX-Clang/main/release/17.x/link.txt)" -O "weebx-clang.tar.gz"
+         mkdir "$HOME"/weebx_clang && tar -xf weebx-clang.tar.gz -C "$HOME"/weebx_clang --strip-components=1 && rm -f weebx-clang.tar.gz "$HOME"/weebx_clang
+	fi
+	export PATH="$HOME/weebx_clang/bin:$PATH"
+	export STRIP="$HOME/weebx_clang/aarch64-linux-gnu/bin/strip"
+	export KBUILD_COMPILER_STRING=$("$HOME"/weebx_clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+fi
 
 # Setup build process
 
 build_kernel() {
 Start=$(date +"%s")
 
+if [ "$TOOLCHAIN" == clang  ]; then
+	echo clang
 	make -j$(nproc --all) O=out \
                               ARCH=arm64 \
-                              LLVM=1 \
-                              LLVM_IAS=1 \
-                              AR=llvm-ar \
-                              NM=llvm-nm \
-                              LD=ld.lld \
-                              OBJCOPY=llvm-objcopy \
-                              OBJDUMP=llvm-objdump \
-                              STRIP=llvm-strip \
-                              CC=clang \
-                              CLANG_TRIPLE=aarch64-linux-gnu- \
-                              CROSS_COMPILE=aarch64-linux-android- \
-                              CROSS_COMPILE_ARM32=arm-linux-androideabi-  2>&1 | tee error.log
+	                      CC="ccache clang" \
+	                      AR=llvm-ar \
+	                      NM=llvm-nm \
+	                      LD=ld.lld \
+	                      AS=llvm-as \
+	                      STRIP=llvm-strip \
+	                      OBJCOPY=llvm-objcopy \
+	                      OBJDUMP=llvm-objdump \
+	                      OBJSIZE=llvm-size \
+	                      READELF=llvm-readelf \
+	                      HOSTCC=clang \
+	                      HOSTCXX=clang++ \
+	                      HOSTAR=llvm-ar \
+	                      LLVM=1 \
+                          LLVM_IAS=1 \
+	                      CROSS_COMPILE=aarch64-linux-gnu- \
+	                      CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+	                      CONFIG_DEBUG_SECTION_MISMATCH=y \
+	                      CONFIG_NO_ERROR_ON_MISMATCH=y   2>&1 | tee error.log
+elif [ "$TOOLCHAIN" == gcc  ]; then
+	echo gcc
+	make -j$(nproc --all) O=out \
+			      ARCH=arm64 \
+			      CROSS_COMPILE=aarch64-elf- \
+			      CROSS_COMPILE_ARM32=arm-eabi- 2>&1 | tee error.log
+fi
 
 End=$(date +"%s")
 Diff=$(($End - $Start))
 }
 
-##------------------------------------------------------##
-
-# Targets | Makefile
-make O=out ARCH=arm64 markw_defconfig
-
-##------------------------------------------------------##
+export IMG="$MY_DIR"/out/arch/arm64/boot/Image.gz-dtb
 
 # Let's start
+
 echo -e "$green << doing pre-compilation process >> \n $white"
 export ARCH=arm64
 export SUBARCH=arm64
@@ -145,7 +186,7 @@ export KBUILD_BUILD_USER="$USEER"
 
 mkdir -p out
 
-make clean && make mrproper
+make O=out clean && make O=out mrproper
 make "$DEFCONFIG" O=out
 
 echo -e "$yellow << compiling the kernel >> \n $white"
@@ -156,17 +197,11 @@ build_kernel || error=true
 DATE=$(date +"%Y%m%d-%H%M%S")
 KERVER=$(make kernelversion)
 
-export IMG="$PWD"/out/arch/arm64/boot/Image.gz
-# export dtbo="$PWD"/out/arch/arm64/boot/dtbo.img
-# export dtb="$PWD"/out/arch/arm64/boot/dtb.img
-
         if [ -f "$IMG" ]; then
                 echo -e "$green << Build completed in $(($Diff / 60)) minutes and $(($Diff % 60)) seconds >> \n $white"
         else
                 echo -e "$red << Failed to compile the kernel , Check up to find the error >>$white"
-                tg_post_msg "Kernel failed to compile uploading error log"
                 tg_error "error.log" "$CHATID"
-                tg_post_msg "done" "$CHATID"
                 rm -rf out
                 rm -rf testing.log
                 rm -rf error.log
@@ -176,19 +211,17 @@ export IMG="$PWD"/out/arch/arm64/boot/Image.gz
 
         if [ -f "$IMG" ]; then
                 echo -e "$green << cloning AnyKernel from your repo >> \n $white"
-                git clone --depth=1 "$AnyKernel" --single-branch -b "$AnyKernelbranch" zip
+                git clone "$AnyKernel" --single-branch -b "$AnyKernelbranch" zip
                 echo -e "$yellow << making kernel zip >> \n $white"
                 cp -r "$IMG" zip/
-                # cp -r "$dtbo" zip/
-                # cp -r "$dtb" zip/
                 cd zip
-                export ZIP="$KERNEL_NAME"-"$KRNL_REL_TAG"-"$CODENAME"
-                zip -r9 "$ZIP" * -x .git README.md LICENSE *placeholder
-                curl -sLo zipsigner-3.0.jar https://github.com/mozzaru/anykernel/raw/master/zipsigner-3.0.jar
+                mv Image.gz-dtb zImage
+                export ZIP="$KERNEL_NAME"-"$CODENAME"-"$DATE"
+                zip -r "$ZIP" *
+                curl -sLo zipsigner-3.0.jar https://raw.githubusercontent.com/mozzaru/anykernel/master/zipsigner-3.0.jar
                 java -jar zipsigner-3.0.jar "$ZIP".zip "$ZIP"-signed.zip
-                tg_post_msg "Kernel successfully compiled uploading ZIP" "$CHATID"
+                tg_post_msg "<b>=============================</b> %0A <b>× Prototype For Redmi 4 Prime ×</b> %0A <b>=============================</b> %0A%0A <b>Date : </b> <code>$(TZ=Indonesia/Jakarta date)</code> %0A%0A <b>Device Code Name:</b> <code>$CODENAME</code> %0A%0A <b>Kernel Version :</b> <code>$KERVER</code> %0A%0A <b>Developer:</b> @mozzaru86 %0A%0A <b> COMPILER :</b> <code>$COMPILER</code> <b> LAST COMMIT :</b> <code>$(git log --pretty=format:'%s' -1)</code> %0A%0A <b>Channel:</b> t.me/Cooking_kernel_bot %0A%0A <b>Changelog:</b> %0A https://github.com/mozzaru/android_kernel_xiaomi_markw_new/commits/master %0A%0A #prototype #markw" "$CHATID"
                 tg_post_build "$ZIP"-signed.zip "$CHATID"
-                tg_post_msg "done" "$CHATID"
                 cd ..
                 rm -rf error.log
                 rm -rf out
